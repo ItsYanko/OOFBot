f380e3867f704a0a225de11f86e45f012711c93c
yanko <ianperina@gmail.com>
2020-06-18 17:32:59 +0100

    Create items.js

---
+++ assets/items.js
@@ -0,0 +1,72 @@
+module.exports = [
+  {
+    ID: "cookie",                                      //Name lowercase w/o spaces
+    price: 10,                                         //OC
+    name: "Cookie",
+    description: "Just a cookie :D",                   //
+    sellable: true, 
+    locked: false,
+    max: 0,
+    icon: "https://cdn.glitch.com/d681b0de-7f03-4ec8-a96f-13605331eac9%2Fcookie.png?v=1589638572051"
+  },
+  {
+    ID: "oofbadge",
+    price: 100,
+    name: "OOF Badge",
+    description: "An cheap nice badge to show in your profile",
+    sellable: true,
+    locked: true,
+    max: 1,
+    icon: "https://cdn.glitch.com/4bfae5c9-a0ad-41ad-ad89-84301f1e44b7%2FOOF%20Badge.png?v=1589909480926"
+  },
+  {
+    ID: "richbadge",
+    price: 1000000,
+    name: "Rich Badge",
+    description: "To flex, if u got $$ for it, it will display on your profile (why would you-)",
+    sellable: true,
+    locked: false,
+    max: 1,
+    icon: "https://cdn.glitch.com/4bfae5c9-a0ad-41ad-ad89-84301f1e44b7%2FRich%20Badge.png?v=1589928547995"
+  },
+  {
+    ID: "test1",
+    price: 1,
+    name: "Test Item 1",
+    description: "Test item",
+    sellable: true,
+    locked: false,
+    max: 1,
+    icon: "https://cdn.glitch.com/4bfae5c9-a0ad-41ad-ad89-84301f1e44b7%2Fquestion_mark.png?v=1589929110962"
+  },
+  {
+    ID: "test2",
+    price: 1,
+    name: "Test Item 2",
+    description: "Test item",
+    sellable: true,
+    locked: false,
+    max: 1,
+    icon: "https://cdn.glitch.com/4bfae5c9-a0ad-41ad-ad89-84301f1e44b7%2Fquestion_mark.png?v=1589929110962"
+  },
+  {
+    ID: "test3",
+    price: 1,
+    name: "Test Item 3",
+    description: "Test item",
+    sellable: true,
+    locked: false,
+    max: 1,
+    icon: "https://cdn.glitch.com/4bfae5c9-a0ad-41ad-ad89-84301f1e44b7%2Fquestion_mark.png?v=1589929110962"
+  },
+  {
+    ID: "test4",
+    price: 1,
+    name: "Test Item 4",
+    description: "Test item",
+    sellable: true,
+    locked: false,
+    max: 1,
+    icon: "https://cdn.glitch.com/4bfae5c9-a0ad-41ad-ad89-84301f1e44b7%2Fquestion_mark.png?v=1589929110962"
+  }
+]
